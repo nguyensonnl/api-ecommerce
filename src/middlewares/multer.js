4b98791cdbe702cc0d42a354fb3a91c3f230cadf
nguyensonnl <nguyenquynhson30446@gmail.com>
2023-04-04 13:40:48 +0700

    update multer.js

--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -26,7 +26,7 @@ const storage = multer.diskStorage({
 const uploadFile = multer({ storage: storage });
 const fs = require("fs");
 const path = require("path");
-const uploadDir = path.join(__dirname, "public", "uploads");
+const uploadDir = path.join(__dirname, "..", "..", "public", "uploads");
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
 }
