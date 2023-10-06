af87a713c8205fc75f9e8bcaad05e8e14078d800
nguyensonnl <nguyenquynhson30446@gmail.com>
2023-10-06 20:45:17 +0700

    Update file index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 //Add headers before the routes are defined
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_APP);
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   // Request methods you wish to allow
   res.setHeader(
